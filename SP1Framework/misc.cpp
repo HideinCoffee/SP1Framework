e400616750b4436d2fa0cdbf511ad9a14e8063c4
Sofrenza <proxiflame@gmail.com>
2020-08-24 15:37:50 +0800

    Attempted to add in a coin function under Misc, set to add 1 to coin counter and replace coin pixel with a blank one. (Under misc.cpp) and edited map slightly. Nothing much.

--- SP1Framework/misc.cpp
+++ SP1Framework/misc.cpp
@@ -1,6 +1,14 @@
 #include "misc.h"
-misc::misc() {
+#include "entity.h"
+#include "global.h"
 
+misc::misc() {
+	int coin = 0;
+	if ((entityarray[0]->getX() << entityarray[0]->getY()) == '$')
+	{
+		coin ++ ;
+		(entityarray[0]->getX() << '@' << entityarray[0]->getY());
+	}
 }
 misc::~misc() {
 	//deconstructor 

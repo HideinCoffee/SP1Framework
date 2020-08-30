de60c5c15e26950dbbe1f6d5d572eb90b00075d8
HideinCoffee <56944682+HideinCoffee@users.noreply.github.com>
2020-08-30 22:08:33 +0800

    made quit button on deathscreen

--- SP1Framework/Renderyoudied.cpp
+++ SP1Framework/Renderyoudied.cpp
@@ -7,7 +7,9 @@
 void renderdeathscreen(Console& g_Console, SMouseEvent& g_mouseEvent, EGAMESTATES& g_eGameState,Map& map) {
 	rectangle(g_Console, 0, 0, 160, 40, ' ', 0x99, 0x99); //background 
 	rectangle(g_Console, 70, 25, 20, 5, ' ', 0x66, 0x77); // retry button
-	rendertext(g_Console, 77, 27, "RETRY", 0x80);
+	rendertext(g_Console, 77, 27, "RETRY", 0x70);
+	rectangle(g_Console, 70, 15, 20, 5, ' ', 0x66, 0x77); // quit button
+	rendertext(g_Console, 77, 17, "QUIT", 0x70);
 	switch (g_mouseEvent.eventFlags) {
 	case 0:
 		// retry button
@@ -17,6 +19,9 @@ void renderdeathscreen(Console& g_Console, SMouseEvent& g_mouseEvent, EGAMESTATE
 			playerarray[0] = new Player(map, BULLETYPE::B_P, 0, 5, 5, 30, 5);
 			//memset(g_skKeyEvent, 0, K_COUNT * sizeof(*g_skKeyEvent));
 			map.slotmap("Tutorial.csv", g_Console);
+		}
+		if ((g_mouseEvent.buttonState == FROM_LEFT_1ST_BUTTON_PRESSED) && buttonpress(g_mouseEvent, 70, 15, 20, 5)) {
+			g_eGameState = EGAMESTATES::S_QUIT;
 		}
 	}
 }
